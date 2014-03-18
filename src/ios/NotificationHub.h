6736d78a08a977fc803757942fa36bbf94cdc806
sgrebnov <sergei.grebnov@gmail.com>
2014-03-18 13:04:18 +0400

    implementation for iOS

---
+++ src/ios/NotificationHub.h
@@ -0,0 +1,39 @@
+﻿/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements. See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership. The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied. See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+
+// Exposes Windows Azure Notification Hubs functionality (Apache Cordova Plugin).
+// http://msdn.microsoft.com/en-us/library/microsoft.windowsazure.messaging.notificationhub.notificationhub.aspx
+@interface NotificationHub : CDVPlugin
+
+@property NSString *callbackId;
+@property NSString *notificationHubPath;
+@property NSString *connectionString;
+
+// Asynchronously registers the device for native notifications.
+- (void)registerApplication:(CDVInvokedUrlCommand*)command;
+// Asynchronously unregisters the native registration on the application or secondary tiles.
+- (void)unregisterApplication:(CDVInvokedUrlCommand*)command;
+
+- (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSNotification *)notif;
+- (void)didFailToRegisterForRemoteNotificationsWithError:(NSNotification *)notif;
+- (void)didReceiveRemoteNotification:(NSNotification *)notif;
+
+@end
+
---
+++ src/ios/NotificationHub.m
@@ -0,0 +1,146 @@
+﻿/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements. See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership. The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied. See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
+#import "NotificationHub.h"
+#import <WindowsAzureMessaging/WindowsAzureMessaging.h>
+
+@implementation NotificationHub {
+
+}
+
+- (void)pluginInitialize {
+    // apply to our custom events since only AppDelegate can receive corresponding events
+    // see iOS platform Quirks on plugin home page to get further instructions
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didRegisterForRemoteNotificationsWithDeviceToken:)
+                                                 name:@"UIApplicationDidRegisterForRemoteNotifications" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didFailToRegisterForRemoteNotificationsWithError:)
+                                                 name:@"UIApplicationDidFailToRegisterForRemoteNotifications" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didReceiveRemoteNotification:)
+                                                 name:@"UIApplicationDidReceiveRemoteNotification" object:nil];
+}
+
+- (void)registerApplication:(CDVInvokedUrlCommand*)command
+{
+    self.notificationHubPath = [command.arguments objectAtIndex:0];
+    self.connectionString = [command.arguments objectAtIndex:1];
+    
+    self.callbackId = command.callbackId;
+    
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes: UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound];
+    
+}
+
+- (void)unregisterApplication:(CDVInvokedUrlCommand*)command
+{
+    self.callbackId = command.callbackId;
+    
+    NSString *notificationHubPath = [command.arguments objectAtIndex:0];
+    NSString *connectionString = [command.arguments objectAtIndex:1];
+    
+    SBNotificationHub* hub = [[SBNotificationHub alloc] initWithConnectionString:connectionString notificationHubPath:notificationHubPath];
+    
+    [hub unregisterNativeWithCompletion:^(NSError* error) {
+        if (error != nil) {
+            [self failWithError:error];
+            return;
+        }
+        [self reportResult:nil keepCallback:[NSNumber numberWithInteger: FALSE]];
+    }];
+    
+}
+
+- (void) didRegisterForRemoteNotificationsWithDeviceToken:(NSNotification *)notif
+{
+    if (self.connectionString == nil || self.notificationHubPath == nil) return;
+    
+    NSData *deviceToken  = notif.object;
+    
+    SBNotificationHub* hub = [[SBNotificationHub alloc] initWithConnectionString:
+                              self.connectionString notificationHubPath:self.notificationHubPath];
+
+    [hub registerNativeWithDeviceToken:deviceToken tags:nil completion:^(NSError* error) {
+        if (error != nil) {
+            [self failWithError:error];
+            return;
+        }
+        
+        // http://stackoverflow.com/a/1587441
+        NSString *channelUri = [[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+        channelUri = [channelUri stringByReplacingOccurrencesOfString:@" " withString:@""];
+        
+        // create callback argument
+        NSMutableDictionary* registration = [NSMutableDictionary dictionaryWithCapacity:4];
+        [registration setObject:@"registerApplication" forKey:@"event"];
+        [registration setObject:channelUri forKey:@"registrationId"]; // TODO: find the way to report registrationId
+        [registration setObject:channelUri forKey:@"channelUri"];
+        [registration setObject:self.notificationHubPath forKey:@"notificationHubPath"];
+        
+        [self reportResult: registration keepCallback:[NSNumber numberWithInteger: TRUE]];
+    }];
+}
+
+- (void)didFailToRegisterForRemoteNotificationsWithError:(NSNotification *)notif
+{
+    NSError* error = notif.object;
+    [self failWithError:error];
+}
+
+- (void)didReceiveRemoteNotification:(NSNotification *)notif
+{
+    NSDictionary* userInfo = notif.object;
+    NSDictionary* apsInfo = [userInfo objectForKey:@"aps"];
+    
+    [self reportResult: apsInfo keepCallback:[NSNumber numberWithInteger: TRUE]];
+}
+
+-(void)reportResult:(NSDictionary*)result keepCallback:(NSNumber*)keepCalback
+{
+    if (self.callbackId == nil) return;
+    
+    CDVPluginResult* pluginResult;
+    if (result != nil)
+    {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary: result];
+    } else
+    {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    pluginResult.keepCallback = keepCalback;
+    
+    [self success:pluginResult callbackId:self.callbackId];
+}
+
+-(void)failWithError:(NSError *)error
+{
+    if (self.callbackId == nil) return;
+    
+    NSString *errorMessage = [error localizedDescription];
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
+    
+    [self.commandDelegate sendPluginResult:commandResult callbackId:self.callbackId];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+
+@end
